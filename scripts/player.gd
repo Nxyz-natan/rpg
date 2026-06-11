1393747a49a8601c74c295c20f87d37d9a0d3879
Nxyz-natan <natanyemane75@icloud.com>
2026-06-11 01:49:30 -0600

    final movment did 8 direction movment fixed the idle animation and tidied up the code

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+@export var SPEED = 100.0
 var current_dir = "none"
 
 func _physics_process(delta):
 	player_movement(delta)
 
 func player_movement(delta):
+	
 	
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
